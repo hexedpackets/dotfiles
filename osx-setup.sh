8c637ea4e8ec3cc4da9fd194b98ce54f51e2615f
William Huba <hexedpackets@gmail.com>
2015-07-23 14:20:56 -0400

    Shell script to install brew and run it.

---
+++ osx-setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+# Install brew bundle and run it
+brew tap Homebrew/bundle
+brew bundle
+
+# Ruby
+rbenv install 2.2.2
